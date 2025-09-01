921de98a7f20ae3791212edd278c7f9e88fad53c
delmack <victor.macioski@hotmail.com>
2025-09-01 18:14:52 -0300

    333
    333

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return render_template('casamento.html')  # ou seu template
+    return render_template('casamento.html')
 
 if __name__ == '__main__':
     app.run(debug=True)
