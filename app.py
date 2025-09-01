a36627ef92a02234836893790e68969822e98844
delmack <victor.macioski@hotmail.com>
2025-09-01 18:00:13 -0300

    vai
    vai

--- app.py
+++ app.py
@@ -1,11 +1,10 @@
-# app.py
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return "Hello World!"
+    return render_template('casamento.html')  # ou seu template
 
 if __name__ == '__main__':
     app.run(debug=True)
